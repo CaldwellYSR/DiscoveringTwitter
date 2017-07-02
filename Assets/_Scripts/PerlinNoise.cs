7aab9822c264fb1b02b1c6750acfe9d14c510c58
Matthew Caldwell <caldwellysr@gmail.com>
2017-07-01 22:38:25 -0400

    Initial Commit - Perlin Noise Generator Working

---
+++ Assets/_Scripts/PerlinNoise.cs
@@ -0,0 +1,46 @@
+﻿/**
+* Perlin Noise Generator: 
+*      Seeded random number generator. Takes integer and returns random number based on that integer.
+*      Given the same "seed" the noise should always be the same.
+* @see http://www.arendpeter.com/Perlin_Noise.html
+*/
+
+using System;
+using UnityEngine;
+
+public class PerlinNoise
+{
+    private long seed;
+
+    public PerlinNoise(long seed)
+    {
+        this.seed = seed;
+    }
+
+    public int GetNoise(int x, int range)
+    {
+        // x == 6
+
+        int frequency = 16;
+        int chunkIndex = x / frequency; // Integer math floors this
+        Debug.Log("Chunk Index: " + chunkIndex);
+        float progress = (x % frequency) / (frequency * 1f);
+        Debug.Log("Progress: " + progress);
+        float leftRandom = this.random(chunkIndex, range);
+        Debug.Log("Left Random: " + leftRandom);
+        float rightRandom = this.random(chunkIndex + 1, range);
+        Debug.Log("Right Random: " + rightRandom);
+
+        float noise = (1 - progress) * leftRandom + progress * rightRandom;
+        Debug.Log("Noise: " + noise);
+
+        return Mathf.RoundToInt(noise);
+
+    }
+
+    private float random(int index, int range)
+    {
+        Debug.Log("Random: " + Mathf.Pow(index + this.seed, 5f) % range);
+        return (Mathf.Pow(index + this.seed, 5f) % range);
+    }
+}
---
+++ Assets/_Scripts/WorldGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+
+    public GameObject DirtPrefab;
+    public GameObject GrassPrefab;
+
+    private int minX = -16;
+    private int maxX = 16;
+
+    private int minY = -10;
+    private int maxY = 10;
+
+    PerlinNoise noise;
+
+	void Start ()
+    {
+        long seed = 3827138L;
+        noise = new PerlinNoise(seed);
+        this.Regenerate();
+	}
+
+    private void Regenerate()
+    {
+        float width = DirtPrefab.transform.lossyScale.x;
+        float height = DirtPrefab.transform.lossyScale.y;
+
+        for (int x = minX; x < maxX; x++) 
+        {
+            int columnHeight = minY + noise.GetNoise(x - minX, maxY - minY);
+            for (int y = minY; y < columnHeight; y++)
+            {
+                GameObject.Instantiate(DirtPrefab, new Vector2(x * width, y * height), Quaternion.identity);
+            }
+        }
+    }
+	
+}
